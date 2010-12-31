aa3d560a15e2ef76f69e40a5ca904c03cad3160c
Aurélien Gâteau <agateau@kde.org>
2010-12-31 16:14:48 +0100

    Stupid

--- deveba/utils.py
+++ deveba/utils.py
@@ -4,7 +4,7 @@ PROGNAME = "deveba"
 HOSTNAME = socket.gethostname()
 
 def generate_commit_message(group):
-    msg = "Automatic commit from %s, running on %s (group %s)" % (PROGNAME, HOSTNAME, group)
+    return "Automatic commit from %s, running on %s (group %s)" % (PROGNAME, HOSTNAME, group)
 
 def get_commit_author_name():
     return PROGNAME
