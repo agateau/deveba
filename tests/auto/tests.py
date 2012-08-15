36a8244235275066cd4b7b0c13e160f001ed226d
Aurélien Gâteau <agateau@kde.org>
2012-08-15 23:53:07 +0200

    Move auto tests to an 'auto' dir

--- tests/auto/tests.py
+++ tests/auto/tests.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "deveba"))
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "deveba"))
 
 from githandlertestcase import GitRepoTestCase, GitHandlerTestCase
 from configtestcase import ConfigTestCase
