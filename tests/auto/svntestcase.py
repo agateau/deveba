956b15e44742789e3de0997507be2ad99ba20db9
Aurelien Gateau <mail@agateau.com>
2018-11-18 10:51:54 +0100

    Factorize test code

--- tests/auto/svntestcase.py
+++ tests/auto/svntestcase.py
@@ -8,26 +8,34 @@ from path import Path
 from deveba.svnhandler import SvnHandler
 from deveba.userinterface import UserInterface
 
+
 def write_file(name, content=""):
     with open(name, "wt") as f:
         f.write(content)
+
 
 def create_remote_repo(repo_dir):
     Path(repo_dir).mkdir()
     run(["svnadmin", "create", repo_dir])
 
+
 def checkout(remote_repo_dir, local_repo_dir):
     run(["svn", "checkout", "file://" + remote_repo_dir, local_repo_dir])
+
+
+def create_test_setup(base_dir):
+    remote_repo_dir = Path(base_dir) / "remote"
+    create_remote_repo(remote_repo_dir)
+
+    local_repo_dir = Path(base_dir) / "local"
+    checkout(remote_repo_dir, local_repo_dir)
+    return remote_repo_dir, local_repo_dir
 
 
 class SvnTestCase(unittest.TestCase):
     def test_new_file(self):
         with TemporaryDirectory() as tmpdirname:
-            remote_repo_dir = Path(tmpdirname) / "remote"
-            create_remote_repo(remote_repo_dir)
-
-            local_repo_dir = Path(tmpdirname) / "local"
-            checkout(remote_repo_dir, local_repo_dir)
+            remote_repo_dir, local_repo_dir = create_test_setup(tmpdirname)
 
             handler = SvnHandler.create(local_repo_dir, options=None)
             self.assertIsNotNone(handler)
@@ -40,12 +48,7 @@ class SvnTestCase(unittest.TestCase):
     def test_update(self):
         with TemporaryDirectory() as tmpdirname:
             ui = UserInterface()
-            remote_repo_dir = Path(tmpdirname) / "remote"
-            create_remote_repo(remote_repo_dir)
-
-            # Create a first checkout
-            local_repo1_dir = Path(tmpdirname) / "local1"
-            checkout(remote_repo_dir, local_repo1_dir)
+            remote_repo_dir, local_repo1_dir = create_test_setup(tmpdirname)
 
             # Create a second checkout, add a file
             local_repo2_dir = Path(tmpdirname) / "local2"
