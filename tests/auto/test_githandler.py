13bc485023c480552f370ab4022fd02a76064505
Aurelien Gateau <mail@agateau.com>
2024-07-06 18:11:57 +0200

    Improve GitHandler test coverage

--- tests/auto/test_githandler.py
+++ tests/auto/test_githandler.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 from pathlib import Path
 
-import pytest
 
+from deveba.gitrepo import GitRepo
 from deveba.userinterface import SilentUserInterface
 from deveba.githandler import GitHandler
 from tests.auto.utils import create_repository
@@ -23,51 +23,69 @@ class FakeUserInterface(SilentUserInterface):
         return self.question_answers.pop(0)
 
 
-class TestGitHandler:
-    @pytest.fixture(autouse=True)
-    def setup_sandbox(self, tmp_path: Path):
-        _, _, self.repository = create_repository(tmp_path)
+def test_sync(tmp_path: Path):
+    _, _, repo = create_repository(tmp_path)
 
-    def test_sync(self):
-        # GIVEN a commit containing the file "modified"
-        modified_path = self.repository.path / "modified"
-        modified_path.touch()
+    # GIVEN a commit containing the file "modified"
+    modified_path = repo.path / "modified"
+    modified_path.touch()
 
-        self.repository.add("modified")
-        self.repository.commit("commit")
+    repo.add("modified")
+    repo.commit("commit")
 
-        # AND "modified" has been modified after the commit
-        modified_path.write_text("foo")
+    # AND "modified" has been modified after the commit
+    modified_path.write_text("foo")
 
-        # AND a file "new" has been created after the commit
-        new_path = self.repository.path / "new"
-        new_path.touch()
+    # AND a file "new" has been created after the commit
+    new_path = repo.path / "new"
+    new_path.touch()
 
-        # THEN repository.get_status() reports them with their correct status
-        status = self.repository.get_status()
-        assert status.modified_files == ["modified"]
-        assert status.new_files == ["new"]
+    # THEN repository.get_status() reports them with their correct status
+    status = repo.get_status()
+    assert status.modified_files == ["modified"]
+    assert status.new_files == ["new"]
 
-        diff = self.repository.run_git("diff")
+    diff = repo.run_git("diff")
 
-        # WHEN syncing the repository
-        handler = GitHandler(self.repository.path)
-        ui = FakeUserInterface()
-        ui.add_question_answer("Show Diff")
-        ui.add_question_answer("Commit")
-        handler.sync(ui)
+    # WHEN syncing the repository
+    handler = GitHandler(repo.path)
+    ui = FakeUserInterface()
+    ui.add_question_answer("Show Diff")
+    ui.add_question_answer("Commit")
+    handler.sync(ui)
 
-        # THEN repository.get_status() does not report any changes
-        status = self.repository.get_status()
-        assert not status.has_changes()
+    # THEN repository.get_status() does not report any changes
+    status = repo.get_status()
+    assert not status.has_changes()
 
-        # AND the log contains a report of the changes
-        assert (
-            ui.log_verbose_calls.pop(0)
-            == "Modified files:\n- modified\n\nNew files:\n- new\n"
-        )
-        assert ui.log_verbose_calls.pop(0) == diff
+    # AND the log contains a report of the changes
+    assert (
+        ui.log_verbose_calls.pop(0)
+        == "Modified files:\n- modified\n\nNew files:\n- new\n"
+    )
+    assert ui.log_verbose_calls.pop(0) == diff
 
-    def test_str(self):
-        handler = GitHandler(self.repository.path)
-        assert isinstance(str(handler), str)
+
+def test_merge_upstream_changes(tmp_path: Path):
+    # GIVEN a repo and a remote repo
+    _, remote_repo, repo = create_repository(tmp_path)
+
+    # AND the remote repo has received new changes
+    repo2_new_content = "hello from repo2"
+    repo2 = GitRepo.clone(remote_repo.path, tmp_path / "repo2")
+    (repo2.path / "new").write_text(repo2_new_content)
+    repo2.add("new")
+    repo2.commit("changes!")
+    repo2.run_git("push")
+
+    # WHEN syncing the repository
+    handler = GitHandler(repo.path)
+    handler.sync(SilentUserInterface())
+
+    # THEN the new changes are received
+    assert (repo.path / "new").read_text() == repo2_new_content
+
+
+def test_str(tmp_path: Path):
+    handler = GitHandler(tmp_path)
+    assert isinstance(str(handler), str)
