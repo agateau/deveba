caaeff4720207b54b59ae3d63dd6f25c9808ffb7
Aurelien Gateau <mail@agateau.com>
2024-07-06 15:17:36 +0200

    svntestcase: use pathlib, port to Pytest

--- tests/auto/svntestcase.py
+++ tests/auto/svntestcase.py
@@ -1,35 +1,34 @@
 # -*- coding: UTF-8 -*-
-import unittest
+import shutil
 from subprocess import run
-from tempfile import TemporaryDirectory
 
-from path import Path
+from pathlib import Path
 
 from deveba.svnhandler import SvnHandler
 from deveba.userinterface import UserInterface
 
 
 def create_remote_repo(repo_dir):
-    Path(repo_dir).mkdir()
+    repo_dir.mkdir()
     run(["svnadmin", "create", repo_dir])
 
 
 def checkout(remote_repo_dir, local_repo_dir):
-    run(["svn", "checkout", "file://" + remote_repo_dir, local_repo_dir])
+    run(["svn", "checkout", f"file://{remote_repo_dir}", local_repo_dir])
 
 
 def create_files(repo_dir, files):
     for relative_path in files:
         file_path = repo_dir / relative_path
-        file_path.dirname().makedirs_p()
+        file_path.parent.mkdir(parents=True, exist_ok=True)
         file_path.touch()
 
 
-def create_test_setup(base_dir, files=None):
-    remote_repo_dir = Path(base_dir) / "remote"
+def create_test_setup(base_dir: Path, files=None):
+    remote_repo_dir = base_dir / "remote"
     create_remote_repo(remote_repo_dir)
 
-    local_repo_dir = Path(base_dir) / "_local"
+    local_repo_dir = base_dir / "_local"
     checkout(remote_repo_dir, local_repo_dir)
     if files:
         create_files(local_repo_dir, files)
@@ -40,57 +39,56 @@ def create_test_setup(base_dir, files=None):
 def run_svn_handler(repo_dir):
     ui = UserInterface()
     handler = SvnHandler.create(repo_dir, options=None)
+    assert handler
     handler.sync(ui)
 
 
-class SvnTestCase(unittest.TestCase):
-    def test_new_file(self):
-        with TemporaryDirectory() as tmpdirname:
-            remote_repo_dir, local_repo_dir = create_test_setup(tmpdirname)
+def test_new_file(tmp_path: Path):
+    remote_repo_dir, local_repo_dir = create_test_setup(tmp_path)
 
-            handler = SvnHandler.create(local_repo_dir, options=None)
-            self.assertIsNotNone(handler)
+    handler = SvnHandler.create(local_repo_dir, options=None)
+    assert handler
 
-            (local_repo_dir / "foo").touch()
+    (local_repo_dir / "foo").touch()
 
-            run_svn_handler(local_repo_dir)
+    run_svn_handler(local_repo_dir)
 
-    def test_update(self):
-        with TemporaryDirectory() as tmpdirname:
-            remote_repo_dir, local_repo1_dir = create_test_setup(tmpdirname)
 
-            # Create a second checkout, add files
-            local_repo2_dir = Path(tmpdirname) / "local2"
-            checkout(remote_repo_dir, local_repo2_dir)
-            create_files(local_repo2_dir, ["foo", "bar/baz"])
-            run_svn_handler(local_repo2_dir)
+def test_update(tmp_path: Path):
+    remote_repo_dir, local_repo1_dir = create_test_setup(tmp_path)
 
-            # Update first checkout
-            run_svn_handler(local_repo1_dir)
+    # Create a second checkout, add files
+    local_repo2_dir = tmp_path / "local2"
+    checkout(remote_repo_dir, local_repo2_dir)
+    create_files(local_repo2_dir, ["foo", "bar/baz"])
+    run_svn_handler(local_repo2_dir)
 
-            # First checkout must contain new files
-            self.assertTrue((local_repo1_dir / "foo").exists())
-            self.assertTrue((local_repo1_dir / "bar/baz").exists())
+    # Update first checkout
+    run_svn_handler(local_repo1_dir)
 
-    def test_remove(self):
-        with TemporaryDirectory() as tmpdirname:
-            # Create a repository with a file in it
-            remote_repo_dir, local_repo1_dir = create_test_setup(
-                tmpdirname, ["foo", "dir1/bar", "dir2/baz"]
-            )
+    # First checkout must contain new files
+    assert (local_repo1_dir / "foo").exists()
+    assert (local_repo1_dir / "bar/baz").exists()
 
-            # Checkout the repository
-            local_repo2_dir = Path(tmpdirname) / "local2"
-            checkout(remote_repo_dir, local_repo2_dir)
 
-            # Rm files from local2
-            (local_repo2_dir / "foo").remove()
-            (local_repo2_dir / "dir1").rmtree()
+def test_remove(tmp_path: Path):
+    remote_repo_dir, local_repo1_dir = create_test_setup(
+        tmp_path, ["foo", "dir1/bar", "dir2/baz"]
+    )
+    # Create a repository with a file in it
 
-            # Sync local2
-            run_svn_handler(local_repo2_dir)
+    # Checkout the repository
+    local_repo2_dir = tmp_path / "local2"
+    checkout(remote_repo_dir, local_repo2_dir)
 
-            # Sync local1, the file should be gone
-            run_svn_handler(local_repo1_dir)
-            self.assertFalse((local_repo1_dir / "foo").exists())
-            self.assertFalse((local_repo1_dir / "dir1").exists())
+    # Rm files from local2
+    (local_repo2_dir / "foo").unlink()
+    shutil.rmtree(local_repo2_dir / "dir1")
+
+    # Sync local2
+    run_svn_handler(local_repo2_dir)
+
+    # Sync local1, the file should be gone
+    run_svn_handler(local_repo1_dir)
+    assert not (local_repo1_dir / "foo").exists()
+    assert not (local_repo1_dir / "dir1").exists()
