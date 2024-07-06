d88d08afc86699b8a7e7dd7aa9b90a13adb20617
Aurelien Gateau <mail@agateau.com>
2024-07-06 17:45:59 +0200

    Improve run() error handling

--- deveba/run.py
+++ deveba/run.py
@@ -1,3 +1,4 @@
+import os
 import shlex
 import subprocess
 
@@ -7,27 +8,39 @@ from pathlib import Path
 
 
 class RunError(Exception):
-    def __init__(self, cmd: List[str], proc: subprocess.CompletedProcess):
+    def __init__(
+        self, cmd: List[str], proc: subprocess.CompletedProcess, cwd: str | None
+    ):
         cmd_str = " ".join(shlex.quote(x) for x in cmd)
+        if cwd is None:
+            cwd = os.getcwd()
         msg = (
-            f"Command '{cmd_str}' failed with exit code {proc.returncode}\n"
+            f"Command '{cmd_str}' running in '{cwd}' failed.\n"
+            f"exit code: {proc.returncode}\n"
+            "stdout:\n"
             f"{proc.stdout}"
+            "stderr:\n"
+            f"{proc.stderr}"
         )
         Exception.__init__(self, msg)
 
 
 def run(
-    cmd: list[str | Path], cwd: str | None = None, env: Mapping[str, str] | None = None
+    cmd: list[str | Path],
+    *,
+    cwd: str | None = None,
+    env: Mapping[str, str] | None = None,
+    merge_output: bool = False,
 ) -> subprocess.CompletedProcess:
     str_cmd: list[str] = [str(x) for x in cmd]
     proc = subprocess.run(
         str_cmd,
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
+        stderr=subprocess.STDOUT if merge_output else subprocess.PIPE,
         env=env,
         cwd=cwd,
         text=True,
     )
     if proc.returncode != 0:
-        raise RunError(str_cmd, proc)
+        raise RunError(str_cmd, proc, cwd)
     return proc
