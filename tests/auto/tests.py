cf004912101f5a0bada42e1c9ab93257bfb09896
Aurelien Gateau <mail@agateau.com>
2024-07-06 10:04:13 +0200

    Make tests pass again

--- tests/auto/tests.py
+++ tests/auto/tests.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
+import os
 import unittest
 
 from githandlertestcase import GitRepoTestCase, GitHandlerTestCase
 from configtestcase import ConfigTestCase
 from svntestcase import SvnTestCase
 
+
+def git_env_setup():
+    os.environ["GIT_CONFIG_SYSTEM"] = "/dev/null"
+    os.environ["GIT_CONFIG_GLOBAL"] = "/dev/null"
+    os.environ["GIT_AUTHOR_NAME"] = "DevebaTests"
+    os.environ["GIT_AUTHOR_EMAIL"] = "deveba@example.com"
+
+    os.environ["GIT_COMMITTER_NAME"] = os.environ["GIT_AUTHOR_NAME"]
+    os.environ["GIT_COMMITTER_EMAIL"] = os.environ["GIT_AUTHOR_EMAIL"]
+
+
 def main():
+    git_env_setup()
     unittest.main()
 
 if __name__ == "__main__":
